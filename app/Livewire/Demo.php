28f5d0db10d23261e123bee6a8a05b0512e23ab5
Zé Marcos <contato.marcosrf@gmail.com>
2023-12-07 14:09:19 -0300

    wip

--- app/Livewire/Demo.php
+++ app/Livewire/Demo.php
@@ -5,13 +5,32 @@ namespace App\Livewire;
 use App\Models\User;
 use Livewire\Component;
 use Illuminate\Contracts\View\View;
+use Livewire\Attributes\Url;
+use Livewire\WithPagination;
 
 class Demo extends Component
 {
+
+    use WithPagination;
+
+    #[Url]
+    public string $search = '';
+
+    public function updatingSearch(): void
+    {
+        $this->resetPage();
+    }
+
     public function render(): View
     {
         return view('livewire.demo', [
-            'users' => User::all(),
+            'users' => User::query()
+                ->when($this->search, function ($query) {
+                    $query->where('name', 'like', "%{$this->search}%")
+                        ->orWhere('email', 'like', "%{$this->search}%")
+                        ->orWhere('created_at', 'like', "%{$this->search}%");
+                })
+                ->paginate(8),
         ]);
     }
 }
--- resources/views/livewire/demo.blade.php
+++ resources/views/livewire/demo.blade.php
@@ -1,4 +1,9 @@
 <div>
+
+    <div>
+        <x-text-input class="py-2" wire:model.live="search" placeholder="Search users..." />
+    </div>
+
     <x-table>
         <x-slot name="head">
             <x-table.heading 
@@ -18,7 +23,7 @@
                 class="px-6 py-3 text-sm font-semibold text-left text-gray-900">Created At</x-table.heading>
         </x-slot>
         <x-slot name="body">
-            @foreach ($users as $user)
+            @forelse ($users as $user)
                 <x-table.row>
                     <x-table.cell class="px-6 py-4 whitespace-nowrap">
                         <div class="text-sm font-semibold text-gray-900">{{ $user->id }}</div>
@@ -38,7 +43,14 @@
                         <div class="text-sm text-gray-900">{{ $user->created_at->diffForHumans() }}</div>
                     </x-table.cell>
                 </x-table.row>
-            @endforeach
+            @empty
+                <x-table.row>
+                    <x-table.cell colspan="5" class="px-6 py-4 whitespace-nowrap">
+                        <div class="text-sm font-semibold text-center text-gray-700">No users found.</div>
+                    </x-table.cell>
+                </x-table.row>
+            @endforelse
         </x-slot>
     </x-table>
+    {{ $users->links() }}
 </div>
